c23b6913e1b03430dbd0ed66141738e9f2cca2a9
Shannon Young <shannon.young@sagebase.org>
2020-01-21 21:51:01 -0800

    Add serialization of the collection and base results
    Add test for an assessment result

--- assessmentModel/src/commonMain/kotlin/serialization/Results.kt
+++ assessmentModel/src/commonMain/kotlin/serialization/Results.kt
@@ -1,15 +1,43 @@
 package org.sagebionetworks.assessmentmodel.serialization
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.modules.SerializersModule
 import org.sagebionetworks.assessmentmodel.*
 
-data class CollectionResultObject(override val identifier: String,
-                                  override var pathHistoryResults: MutableList<Result> = mutableListOf(),
-                                  override var asyncActionResults: MutableSet<Result> = mutableSetOf()) : CollectionResult
+val resultSerializersModule = SerializersModule {
+    polymorphic(Result::class) {
+        CollectionResultObject::class with CollectionResultObject.serializer()
+        AssessmentResultObject::class with AssessmentResultObject.serializer()
+        ResultObject::class with ResultObject.serializer()
+        WebViewButtonObject::class with WebViewButtonObject.serializer()
+        VideoViewButtonObject::class with VideoViewButtonObject.serializer()
+    }
+}
 
+
+@Serializable
+@SerialName("collection")
+data class CollectionResultObject(override val identifier: String,
+                                  @SerialName("stepHistory")
+                                  override var pathHistoryResults: MutableList<Result> = mutableListOf(),
+                                  @SerialName("asyncResults")
+                                  override var asyncActionResults: MutableSet<Result> = mutableSetOf()) : CollectionResult
+@Serializable
+@SerialName("assessment")
 data class AssessmentResultObject(override val identifier: String,
                                   override val versionString: String? = null,
+                                  @SerialName("stepHistory")
                                   override var pathHistoryResults: MutableList<Result> = mutableListOf(),
+                                  @SerialName("asyncResults")
                                   override var asyncActionResults: MutableSet<Result> = mutableSetOf(),
+                                  @SerialName("taskRunUUID")
                                   override var taskRunUUIDString: String = UUIDGenerator.uuidString(),
+                                  @SerialName("startDate")
                                   override var startDateString: String = DateGenerator.nowString(),
+                                  @SerialName("endDate")
                                   override var endDateString: String = DateGenerator.nowString()) : AssessmentResult
+
+@Serializable
+@SerialName("base")
+data class ResultObject(override val identifier: String) : Result
--- assessmentModel/src/commonMain/kotlin/serialization/Serialization.kt
+++ assessmentModel/src/commonMain/kotlin/serialization/Serialization.kt
@@ -3,7 +3,7 @@ package org.sagebionetworks.assessmentmodel.serialization
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.plus
 
-val defaultSerialzersModule = imageSerializersModule + buttonSerializersModule
+val defaultSerialzersModule = imageSerializersModule + buttonSerializersModule + resultSerializersModule
 
 val jsonCoder = Json(context = defaultSerialzersModule)
 
