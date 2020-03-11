b5cbc9f45c9aa3c814757f0f048174cf7bc516fa
Shannon Young <shannon.young@sagebase.org>
2020-03-11 14:10:50 -0700

    Update assessmentModel/src/commonMain/kotlin/resourcemanagement/FileLoader.kt
    Co-Authored-By: Erin-Mounts <Erin-Mounts@users.noreply.github.com>

--- assessmentModel/src/commonMain/kotlin/resourcemanagement/FileLoader.kt
+++ assessmentModel/src/commonMain/kotlin/resourcemanagement/FileLoader.kt
@@ -3,7 +3,7 @@ package org.sagebionetworks.assessmentmodel.resourcemanagement
 interface FileLoader {
 
     /**
-     * Apple and Android devices have a different structure and information that is required to load a file.
+     * Apple and Android devices have different structures and information that are required to load a file.
      */
     fun loadFile(resourceName: String, rawFileExtension: String? = null, resourceInfo: ResourceInfo): String
 }
