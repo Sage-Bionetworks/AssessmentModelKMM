b39800fca1476065c809e88d7bdb20ee8669eb62
Nathaniel Brown <nathaniel.brown@sagebase.org>
2020-04-27 10:28:49 -0700

    Debug step fragment that displays string value of step

---
+++ presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/DebugStepFragment.kt
@@ -0,0 +1,57 @@
+package org.sagebionetworks.assessmentmodel.presentation
+
+import android.content.Context
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.sagebionetworks.assessmentmodel.InstructionStep
+import org.sagebionetworks.assessmentmodel.presentation.databinding.DebugStepFragmentBinding
+import org.sagebionetworks.assessmentmodel.presentation.databinding.InstructionStepFragmentBinding
+import org.sagebionetworks.assessmentmodel.serialization.loadDrawable
+
+open class DebugStepFragment: StepFragment() {
+
+    private var _binding: DebugStepFragmentBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var step: InstructionStep
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding = DebugStepFragmentBinding.inflate(layoutInflater, container, false)
+        step = stepViewModel.nodeState.node as InstructionStep
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.title.text = step.title
+        binding.detail.text = step.detail
+        binding.raw.text = step.toString()
+        val drawable = step.imageInfo?.loadDrawable(context!!)
+        binding.header.image.setImageDrawable(drawable)
+        if (drawable is AnimationDrawable) {
+            drawable.start()
+        }
+        binding.navBar.navBarNext.setOnClickListener { assessmentViewModel.goForward() }
+        binding.navBar.navBarBack.setOnClickListener { assessmentViewModel.goBackward() }
+        binding.header.closeBtn.setOnClickListener{ assessmentViewModel.cancel() }
+    }
+
+}
