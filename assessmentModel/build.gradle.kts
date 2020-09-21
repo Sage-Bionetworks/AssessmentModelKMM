231f3fabc71ef9eb507af194e4983296d6c21381
Nathaniel Brown <nathaniel.brown@sagebase.org>
2020-09-21 10:42:34 -0700

    Update kotlin and serialization versions

--- assessmentModel/build.gradle.kts
+++ assessmentModel/build.gradle.kts
@@ -69,7 +69,7 @@ kotlin {
    sourceSets {
       commonMain {
          dependencies {
-             api ("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
+             api ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
          }
       }
       commonTest {
@@ -79,11 +79,11 @@ kotlin {
          }
       }
        sourceSets["androidLibMain"].dependencies {
-           api( "org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
+           api( "org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
            implementation("androidx.appcompat:appcompat:1.1.0")
        }
        sourceSets["iosMain"].dependencies {
-           api( "org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
+           api( "org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
        }
 
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.10")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
     }
 }
