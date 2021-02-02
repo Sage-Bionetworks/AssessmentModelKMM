9e483799181134426a089b03f9cb13ce12879d62
Rachel Ye <rye@SageITs-MacBook-Pro.local>
2021-02-02 11:34:01 -0800

    working on multiple input fragment

--- presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/MultipleInputQuestionStepFragment.kt
+++ presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/MultipleInputQuestionStepFragment.kt
@@ -6,34 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import org.sagebionetworks.assessmentmodel.Step
 import org.sagebionetworks.assessmentmodel.navigation.NodeState
+import org.sagebionetworks.assessmentmodel.presentation.databinding.TextQuestionStepFragmentBinding
+import org.sagebionetworks.assessmentmodel.survey.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [MultipleInputQuestionStepFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MultipleInputQuestionStepFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    protected lateinit var stepViewModel: StepViewModel
-    protected lateinit var nodeState: NodeState
+class MultipleInputQuestionStepFragment : StepFragment() {
 
-    protected lateinit var assessmentViewModel: AssessmentViewModel
+    private var _binding: TextQuestionStepFragmentBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        assessmentViewModel = (parentFragment as AssessmentFragment).viewModel
-        stepViewModel = ViewModelProvider(
-            this, StepViewModelFactory()
-                .create(assessmentViewModel.currentNodeStateLiveData.value!!.nodeState))
-            .get(StepViewModel::class.java)
-        nodeState = stepViewModel.nodeState
-    }
+    lateinit var questionStep: SimpleQuestion
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,23 +33,10 @@ class MultipleInputQuestionStepFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_multiple_input_question_step, container, false)
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment MultipleInputQuestionStepFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            MultipleInputQuestionStepFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.navBar.setForwardOnClickListener { assessmentViewModel.goForward() }
+        binding.navBar.setBackwardOnClickListener { assessmentViewModel.goBackward() }
+        binding.navBar.setup(questionStep as Step)
+    }
 }
