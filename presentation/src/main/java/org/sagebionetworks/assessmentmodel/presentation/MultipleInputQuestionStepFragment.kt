8c0b3b4ce8362426b576acfe0eb407b947c688a4
Rachel Ye <rye@SageITs-MacBook-Pro.local>
2021-02-03 16:24:22 -0800

    updating text view buttons

--- presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/MultipleInputQuestionStepFragment.kt
+++ presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/MultipleInputQuestionStepFragment.kt
@@ -1,14 +1,14 @@
 package org.sagebionetworks.assessmentmodel.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
 import org.sagebionetworks.assessmentmodel.Step
-import org.sagebionetworks.assessmentmodel.navigation.NodeState
-import org.sagebionetworks.assessmentmodel.presentation.databinding.TextQuestionStepFragmentBinding
+import org.sagebionetworks.assessmentmodel.presentation.databinding.FragmentMultipleInputQuestionStepBinding
+import org.sagebionetworks.assessmentmodel.serialization.ChoiceQuestionObject
+import org.sagebionetworks.assessmentmodel.serialization.SimpleQuestionObject
 import org.sagebionetworks.assessmentmodel.survey.*
 
 
@@ -19,24 +19,61 @@ import org.sagebionetworks.assessmentmodel.survey.*
  */
 class MultipleInputQuestionStepFragment : StepFragment() {
 
-    private var _binding: TextQuestionStepFragmentBinding? = null
+    private var _binding: FragmentMultipleInputQuestionStepBinding? = null
     // This property is only valid between onCreateView and onDestroyView.
     val binding get() = _binding!!
 
-    lateinit var questionStep: SimpleQuestion
+    lateinit var questionStep: MultipleInputQuestion
+    lateinit var questionState: QuestionState
+    lateinit var inputState: KeyboardInputItemState<*>
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    lateinit var inputStatesList: List<InputItemState>
+    lateinit var inputItemsList : List<InputItem>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        questionState = nodeState as QuestionState
+        questionStep = questionState.node as MultipleInputQuestion
+        inputStatesList = questionState.itemStates
+
+        inputState = inputStatesList[2] as KeyboardInputItemState<*>
+        inputItemsList = questionStep.inputItems
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multiple_input_question_step, container, false)
+        _binding = FragmentMultipleInputQuestionStepBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        binding.navBar.setForwardOnClickListener { assessmentViewModel.goForward() }
+        binding.navBar.setForwardOnClickListener {
+//            binding.question2Input.updateResult(inputState)
+//            questionState.saveAnswer(inputState.currentAnswer, inputState)
+            assessmentViewModel.goForward()
+        }
         binding.navBar.setBackwardOnClickListener { assessmentViewModel.goBackward() }
         binding.navBar.setup(questionStep as Step)
+        binding.questionHeader.questionTitle.text = questionStep.title
+        binding.questionHeader.questionSubtitle.text = questionStep.subtitle
+        binding.questionHeader.closeBtn.setOnClickListener{ assessmentViewModel.cancel() }
+//        for (inputItem in inputItemsList) {
+//            if (inputItem.equals(ChoiceQuestionObject)) { //multiple choice question
+//                binding.question0Input.setup(questionState)
+//                binding.question1Input.setup(questionState)
+//            } else if (inputItem.equals(SimpleQuestionObject)) { //text input question
+//                binding.question2Input.setup(inputState)
+//            }
+//            break
+//        }
+        binding.question0Input.setup(inputState)
+        binding.question1Input.setup(inputState)
+        binding.question2Input.setup(inputState)
     }
 }
