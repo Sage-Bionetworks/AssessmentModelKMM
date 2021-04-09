1d8b28d74d23dadfffc9002d13c61db4e0567924
Rachel Ye <rye@SageITs-MBP.agd.com>
2021-04-09 00:35:16 -0700

    added the json placeholder text for the checkbox questions

--- presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/CheckboxFragment.kt
+++ presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/CheckboxFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.koin.android.ext.android.bind
 import org.sagebionetworks.assessmentmodel.Step
 import org.sagebionetworks.assessmentmodel.presentation.databinding.CheckboxFragmentBinding
 import org.sagebionetworks.assessmentmodel.survey.*
@@ -18,14 +19,25 @@ class CheckboxFragment : StepFragment() {
     private var _binding: CheckboxFragmentBinding? = null
     val binding get() = _binding!!
 
+    lateinit var questionStep: SimpleQuestion
+    lateinit var questionState: QuestionState
+    lateinit var inputState: KeyboardInputItemState<*>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        questionState = nodeState as QuestionState
+        questionStep = questionState.node as SimpleQuestion
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         _binding = CheckboxFragmentBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -35,9 +47,11 @@ class CheckboxFragment : StepFragment() {
         }
         binding.navBar.setBackwardOnClickListener { assessmentViewModel.goBackward() }
         binding.navBar.setSkipOnClickListener { assessmentViewModel.goForward() }
-        //binding.navBar.setup(questionStep as Step)
-        //binding.questionHeader.questionTitle.text = questionStep.title
-        //binding.questionHeader.questionSubtitle.text = questionStep.subtitle
+        binding.navBar.setup(questionStep as Step)
+        binding.questionHeader.questionTitle.text = questionStep.title
+        binding.questionHeader.questionSubtitle.text = questionStep.subtitle
+        binding.checkBox0.text = questionStep.inputItem.fieldLabel
+        binding.checkBox1.text = questionStep.inputItem.fieldLabel
         binding.questionHeader.closeBtn.setOnClickListener{ assessmentViewModel.cancel() }
     }
 }
