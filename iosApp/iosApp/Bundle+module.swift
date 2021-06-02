9a552600c95ede3ad3508ed8bbb9f9a977a02a41
Shannon Young <shannon.young@sagebase.org>
2021-06-02 11:37:37 -0700

    Update iosApp/iosApp/Bundle+module.swift
    Co-authored-by: Erin-Mounts <Erin-Mounts@users.noreply.github.com>

--- iosApp/iosApp/Bundle+module.swift
+++ iosApp/iosApp/Bundle+module.swift
@@ -35,7 +35,7 @@ import Foundation
 
 // Swift Packages have an internal static property defined on the Bundle to access
 // bundle resources. This code file is *not* included in the swift packages and can
-// allow building both a dynamic framework *or* a Swift Package using the same code
+// allow building either a dynamic framework *or* a Swift Package using the same code
 // files. syoung 11/05/2020
 
 class BundleResource {
