22a127b8be9e6fa9265af1527db9142ead956858
Shannon Young <shannon.young@sagebase.org>
2022-03-07 16:47:42 -0800

    remove recorder configurations that are not used

--- assessmentModel/src/commonMain/kotlin/serialization/AsyncActionConfigurations.kt
+++ assessmentModel/src/commonMain/kotlin/serialization/AsyncActionConfigurations.kt
@@ -2,8 +2,6 @@ package org.sagebionetworks.assessmentmodel.serialization
 
 import kotlinx.serialization.modules.*
 import org.sagebionetworks.assessmentmodel.AsyncActionConfiguration
-import org.sagebionetworks.assessmentmodel.recorders.DistanceRecorderConfiguration
-import org.sagebionetworks.assessmentmodel.recorders.MotionRecorderConfiguration
 
 /**
  * The distance and motion recorders are included within this package to allow for standardized serialization of these
@@ -11,7 +9,5 @@ import org.sagebionetworks.assessmentmodel.recorders.MotionRecorderConfiguration
  */
 val asyncActionSerializersModule = SerializersModule {
     polymorphic(AsyncActionConfiguration::class) {
-        subclass(DistanceRecorderConfiguration::class)// with DistanceRecorderConfiguration.serializer()
-        subclass(MotionRecorderConfiguration::class)// with MotionRecorderConfiguration.serializer()
     }
 }
