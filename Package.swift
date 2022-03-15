5b12c8b38ac473ae7bdf80ad8a3d44db1e804817
Shannon Young <shannon.young@sagebase.org>
2022-03-15 12:05:56 -0700

    update package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "JsonModel",
-                 path: "../JsonModel-Swift"),
+                 url: "https://github.com/Sage-Bionetworks/JsonModel-Swift.git",
+                 from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
