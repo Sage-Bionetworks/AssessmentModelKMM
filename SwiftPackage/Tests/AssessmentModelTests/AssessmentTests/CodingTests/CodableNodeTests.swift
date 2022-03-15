24d66f18b426d26b8604a6c931aba5e84917380c
Shannon Young <shannon.young@sagebase.org>
2022-03-15 12:58:50 -0700

    Fix copyright dates

--- SwiftPackage/Tests/AssessmentModelTests/AssessmentTests/CodingTests/CodableNodeTests.swift
+++ SwiftPackage/Tests/AssessmentModelTests/AssessmentTests/CodingTests/CodableNodeTests.swift
@@ -2,7 +2,7 @@
 //  CodableQuestionTests.swift
 //  ResearchTests_iOS
 //
-//  Copyright © 2020 Sage Bionetworks. All rights reserved.
+//  Copyright © 2020-2022 Sage Bionetworks. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
