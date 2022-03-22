af8e398351d85e9bbb6671e0780def2d11ea37c4
Shannon Young <shannon.young@sagebase.org>
2022-03-22 11:19:35 -0700

    Create a workspace

---
+++ iosApp/kmmTestAppTests/KMMSerializationTests.swift
@@ -0,0 +1,124 @@
+//
+//  KMMSerializationTests.swift
+//
+//
+//  Copyright © 2022 Sage Bionetworks. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2.  Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission. No license is granted to the trademarks of
+// the copyright holders even if such marks are included in this software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+//import XCTest
+//import AssessmentModel
+//import AssessmentModelKMM
+//
+//class KMMSerializationTests: XCTestCase {
+//    
+//    // TODO: syoung 03/21/2022 Support "copyright" in the Kotlin model.
+//    
+//    func testAssessmentCodable() {
+//        let factory = AssessmentFactory()
+//        let encoder = factory.createJSONEncoder()
+//
+//        do {
+//            
+//            let json = try encoder.encode(swiftAssessment)
+//            let jsonString = String(data: json, encoding: .utf8)!
+//            let loader = AssessmentJsonStringLoader(jsonString: jsonString, bundle: Bundle.main)
+//            let kmmAssessment = try loader.decodeObject() as? AssessmentModelKMM.AssessmentObject
+//            XCTAssertNotNil(kmmAssessment)
+//            
+//            guard let kmmAssessment = kmmAssessment else {
+//                XCTFail("Failed to decode assessment from the kotlin model.")
+//                return
+//            }
+//            
+//            XCTAssertEqual(swiftAssessment.identifier, kmmAssessment.identifier)
+//            XCTAssertEqual(swiftAssessment.comment, kmmAssessment.comment)
+//            XCTAssertEqual(swiftAssessment.estimatedMinutes, Int(kmmAssessment.estimatedMinutes))
+//            XCTAssertEqual(swiftAssessment.versionString, kmmAssessment.versionString)
+//            
+//            XCTAssertEqual(swiftAssessment.children.count, kmmAssessment.children.count)
+//            
+//            if let overview = kmmAssessment.children.first as? AssessmentModelKMM.OverviewStepObject {
+//                checkObjects(swiftNode: swiftOverviewStep, kmmNode: overview)
+//            } else {
+//                XCTFail("Failed to decode the overview step as the first step")
+//            }
+//            
+//            if let instruction = kmmAssessment.children[1] as? AssessmentModelKMM.InstructionStepObject {
+//                checkObjects(swiftNode: swiftInstructionStep, kmmNode: instruction)
+//            } else {
+//                XCTFail("Failed to decode the instruction step as the second step")
+//            }
+//            
+//            if let section = kmmAssessment.children[2] as? AssessmentModelKMM.SectionObject {
+//                checkObjects(swiftNode: swiftQuestionSection, kmmNode: section)
+//                swiftQuestionSection.children.enumerated().forEach { node in
+//                    guard let swiftNode = node.element as? AssessmentModel.AbstractQuestionStepObject,
+//                          section.children.count > node.offset,
+//                          let kmmNode = section.children[node.offset] as? AssessmentModelKMM.QuestionObject
+//                    else {
+//                        XCTFail("The kotlin node or swift node does not match expected. at:\(node.offset), for:\(node.element)")
+//                        return
+//                    }
+//                    checkObjects(swiftNode: swiftNode, kmmNode: kmmNode)
+//                    // TODO: check that the questions match. syoung 03/21/2022
+//                }
+//            } else {
+//                XCTFail("Failed to decode the section as the third node")
+//            }
+//            
+//            if let completion = kmmAssessment.children.last as? AssessmentModelKMM.CompletionStepObject {
+//                checkObjects(swiftNode: swiftCompletionStep, kmmNode: completion)
+//            } else {
+//                XCTFail("Failed to decode the completion step as the last step")
+//            }
+//            
+//        } catch {
+//            XCTFail("Failed to encode or decode the assessment: \(error)")
+//        }
+//    }
+//    
+//    func checkObjects(swiftNode: AssessmentModel.AbstractContentNodeObject, kmmNode: AssessmentModelKMM.NodeObject) {
+//        XCTAssertEqual(swiftNode.identifier, kmmNode.identifier)
+//        XCTAssertEqual(swiftNode.title, kmmNode.title)
+//        XCTAssertEqual(swiftNode.subtitle, kmmNode.subtitle)
+//        XCTAssertEqual(swiftNode.detail, kmmNode.detail)
+//        XCTAssertEqual(swiftNode.comment, kmmNode.comment)
+//        XCTAssertEqual(swiftNode.nextNode?.stringValue, kmmNode.nextNodeIdentifier)
+//        XCTAssertEqual(swiftNode.shouldHideButtons.map { $0.stringValue }, kmmNode.hideButtons.map { $0.name })
+//        XCTAssertEqual(swiftNode.buttonMap.count, kmmNode.buttonMap.count)
+//        kmmNode.buttonMap.forEach { (key, value) in
+//            guard let swiftBtn = swiftNode.buttonMap[.init(rawValue: key.name)] else {
+//                XCTFail("\(key.name) not found in the swift button map.")
+//                return
+//            }
+//            XCTAssertEqual(value.buttonTitle, swiftBtn.buttonTitle)
+//            XCTAssertEqual(value.imageInfo?.imageName, swiftBtn.iconName)
+//        }
+//    }
+//}
---
+++ iosApp/kmmTestAppTests/SwiftTestAssessment.swift
@@ -0,0 +1,163 @@
+//
+//  KMMTestAssessment.swift
+//  
+//
+//  Copyright © 2022 Sage Bionetworks. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2.  Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission. No license is granted to the trademarks of
+// the copyright holders even if such marks are included in this software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import Foundation
+import AssessmentModel
+
+
+let swiftFetchableImage = FetchableImage(imageName: "instructionFoo",
+                                    label: "labelFoo",
+                                    placementHint: "topBackground",
+                                    imageSize: .init(width: 100, height: 100))
+let swiftAnimatedImage = AnimatedImage(imageNames: ["image1","image2"], animationDuration: 2.0)
+
+let swiftGoButton = ButtonActionInfoObject(buttonTitle: "Go, Dogs! Go")
+let swiftBackButton = ButtonActionInfoObject(iconName: "back")
+
+let swiftInstructionStep = InstructionStepObject(identifier: "instruction",
+                                            title: "title",
+                                            subtitle: "subtitle",
+                                            detail: "detail",
+                                            imageInfo: swiftFetchableImage,
+                                            shouldHideButtons: [.navigation(.goBackward)],
+                                            buttonMap: [
+                                                .navigation(.goForward) : swiftGoButton,
+                                                .navigation(.goBackward) : swiftBackButton],
+                                            comment: "comment",
+                                            nextNode: .node("baroo"),
+                                            fullInstructionsOnly: true,
+                                            spokenInstructions: [.start : "Start instructions"])
+
+let swiftCompletionStep = CompletionStepObject(identifier: "completion",
+                                          title: "title",
+                                          subtitle: "subtitle",
+                                          detail: "detail",
+                                          imageInfo: swiftAnimatedImage,
+                                          shouldHideButtons: [.navigation(.pause)],
+                                          buttonMap: [.navigation(.goForward) : swiftGoButton],
+                                          comment: "comment",
+                                          nextNode: .reserved(.exit))
+
+let swiftOverviewStep = OverviewStepObject(identifier: "overview",
+                                      title: "title",
+                                      subtitle: "subtitle",
+                                      detail: "detail",
+                                      imageInfo: swiftFetchableImage,
+                                      shouldHideButtons: [.navigation(.pause)],
+                                      buttonMap: [.navigation(.goForward) : swiftGoButton],
+                                      comment: "comment",
+                                      nextNode: .reserved(.nextSection))
+
+let regex = try! NSRegularExpression(pattern: "/[a-z]+/g", options: [])
+let swiftStringTextInputItem = StringTextInputItemObject(fieldLabel: "Question 1",
+                                                         placeholder: "enter string",
+                                                         resultIdentifier: "foo",
+                                                         keyboardOptions: .init(keyboardType: .asciiCapable),
+                                                         regExValidator: .init(pattern: regex, invalidMessage: "Failed regex"))
+
+let swiftStringQuestion = SimpleQuestionStepObject(identifier: "stringQuestion",
+                                                   inputItem: swiftStringTextInputItem)
+
+let swiftIntOptions: IntegerFormatOptions = {
+    var options = IntegerFormatOptions()
+    options.minimumValue = 0
+    options.minimumValue = 10000
+    options.usesGroupingSeparator = false
+    options.stepInterval = 10
+    return options
+}()
+let swiftIntTextInputItem = IntegerTextInputItemObject(fieldLabel: "Question 2",
+                                                  placeholder: "Enter integer",
+                                                  resultIdentifier: "foo",
+                                                  formatOptions: swiftIntOptions)
+let swiftIntQuestion = SimpleQuestionStepObject(identifier: "intQuestion", inputItem: swiftIntTextInputItem)
+
+let swiftDoubleOptions: DoubleFormatOptions = {
+    var options = DoubleFormatOptions()
+    options.minimumValue = -1.0
+    options.maximumValue = 1.0
+    options.maximumFractionDigits = 4
+    return options
+}()
+let swiftDoubleTextInputItem = DoubleTextInputItemObject(fieldLabel: "Question 3",
+                                                         placeholder: "Enter number between -1 and 1",
+                                                         resultIdentifier: "foo",
+                                                         formatOptions: swiftDoubleOptions)
+let swiftDoubleQuestion = SimpleQuestionStepObject(identifier: "doubleQuestion", inputItem: swiftDoubleTextInputItem)
+
+let swiftYearOptions: YearFormatOptions = {
+    var options = YearFormatOptions()
+    options.allowFuture = false
+    options.minimumYear = 2000
+    return options
+}()
+let swiftYearTextInputItem = YearTextInputItemObject(fieldLabel: "Question 4",
+                                                     placeholder: "enter a year",
+                                                     resultIdentifier: "foo",
+                                                     formatOptions: swiftYearOptions)
+let swiftYearQuestion = SimpleQuestionStepObject(identifier: "yearQuestion",
+                                                 inputItem: swiftYearTextInputItem,
+                                                 surveyRules: [.init(skipToIdentifier: "completion", matchingValue: .integer(2009), ruleOperator: .lessThan)])
+
+let swiftChoiceQuestion = ChoiceQuestionStepObject(identifier: "Question 5",
+                                              choices: [
+                                                .init(value: .integer(0), text: "zero", detail: "detail", selectorType: .default),
+                                                .init(value: .integer(1), text: "one", detail: "detail", selectorType: .default),
+                                                .init(value: .integer(2), text: "two", detail: "detail", selectorType: .all),
+                                                .init(value: .integer(3), text: "three", detail: "detail", selectorType: .exclusive),
+                                              ],
+                                              baseType: .integer,
+                                              singleChoice: false,
+                                              other: IntegerTextInputItemObject(),
+                                              title: "title",
+                                              subtitle: "subtitle",
+                                              detail: "detail",
+                                              imageInfo: swiftFetchableImage,
+                                              optional: false,
+                                              uiHint: .init(rawValue: "checkbox"),
+                                              comment: "comment",
+                                              nextNode: .node("question2"))
+
+let swiftQuestionSection = SectionObject(identifier: "questionSection",
+                                         children: [
+                                            swiftChoiceQuestion,
+                                            swiftDoubleQuestion,
+                                            swiftIntQuestion,
+                                            swiftStringQuestion,
+                                            swiftYearQuestion,
+                                         ])
+
+let swiftAssessment = AssessmentObject(identifier: "foo",
+                                       children: [swiftOverviewStep, swiftInstructionStep, swiftQuestionSection, swiftCompletionStep],
+                                       version: "0.0.1",
+                                       estimatedMinutes: 2)
