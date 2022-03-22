1c5397e8f7af766958fed72d616037b0caf6d055
Shannon Young <shannon.young@sagebase.org>
2022-03-22 11:18:10 -0700

    Remove binary from swift package

--- Package.swift
+++ Package.swift
@@ -27,10 +27,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         
-        // TODO: syoung 03/02/2022 Add in the kotlin binary for testing purposes.
-//        .binaryTarget(name: "AssessmentModelKMM",
-//                      path: "SwiftPackage/Binaries/AssessmentModelKMM.xcframework"),
-        
         .target(name: "AssessmentModel",
                 dependencies: [
                     "JsonModel",
@@ -40,7 +36,7 @@ let package = Package(
         .testTarget(
             name: "AssessmentModelTests",
             dependencies: [
-                "AssessmentModel"
+                "AssessmentModel",
             ],
             path: "SwiftPackage/Tests/AssessmentModelTests",
             resources: [
