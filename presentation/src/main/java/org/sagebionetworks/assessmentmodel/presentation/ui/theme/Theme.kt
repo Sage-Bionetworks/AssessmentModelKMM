8329ac53eeaa471a55028e21fadaf05afa03414a
Nathaniel Brown <nathaniel.brown@sagebase.org>
2022-04-20 12:42:28 -0700

    For now show light theme even in dark mode

--- presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/ui/theme/Theme.kt
+++ presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/ui/theme/Theme.kt
@@ -7,7 +7,7 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = BluePrimary,
     primaryVariant = Purple700,
     secondary = Teal200
 )
@@ -33,7 +33,8 @@ fun SageSurveyTheme(
     content: @Composable() () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
+        //TODO: Support dark theme -nbrown 04/19/2022 DarkColorPalette
     } else {
         LightColorPalette
     }
