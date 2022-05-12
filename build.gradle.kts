2af8df9d02c2fea91ff70e843a8fa69c864cedcc
Nathaniel Brown <nathaniel.brown@sagebase.org>
2022-05-12 13:46:43 -0700

    Android version 0.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ tasks.dokkaHtmlMultiModule {
 
 allprojects {
     group = "org.sagebionetworks.assessmentmodel"
-    version = "0.5.0"
+    version = "0.5.2"
 
     repositories {
         google()
