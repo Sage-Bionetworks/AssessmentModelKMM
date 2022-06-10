6c0d7e5b94b6ed7ac94d6a96536223c5eed2c2cd
Shannon Young <shannon.young@sagebase.org>
2022-06-09 17:19:33 -0700

    Allow images to be placed below the text and center aligned

--- SwiftPackage/Sources/AssessmentModelUI/Views/Components/StepHeaderView.swift
+++ SwiftPackage/Sources/AssessmentModelUI/Views/Components/StepHeaderView.swift
@@ -45,9 +45,12 @@ public struct StepHeaderView : View {
     
     public var body: some View {
         HStack {
+            if nodeState.skipStepText == nil && !assessmentState.canPause {
+                Spacer()
+            }
             ExitButton(canPause: assessmentState.canPause)
-            Spacer()
             if let text = nodeState.skipStepText {
+                Spacer()
                 UnderlinedButton(label: text) {
                     nodeState.willSkip()
                     pagedNavigation.goForward()
--- SwiftPackage/Sources/AssessmentModelUI/Views/Steps/instruction/InstructionStepView.swift
+++ SwiftPackage/Sources/AssessmentModelUI/Views/Steps/instruction/InstructionStepView.swift
@@ -37,15 +37,17 @@ import SharedMobileUI
 
 public struct InstructionStepView: View {
     @ObservedObject var nodeState: ContentNodeState
+    let alignment: Alignment
     
-    public init(_ nodeState: ContentNodeState) {
+    public init(_ nodeState: ContentNodeState, alignment: Alignment = .leading) {
         self.nodeState = nodeState
+        self.alignment = alignment
     }
     
     public var body: some View {
         VStack {
             StepHeaderView(nodeState)
-            ContentNodeView(nodeState.contentNode, alignment: .leading)
+            ContentNodeView(nodeState.contentNode, alignment: alignment)
             SurveyNavigationView()
         }
     }
@@ -53,14 +55,25 @@ public struct InstructionStepView: View {
 
 struct InstructionView_Previews: PreviewProvider {
     static var previews: some View {
-        InstructionStepView(InstructionState(example, parentId: nil))
-            .environmentObject(PagedNavigationViewModel(pageCount: 5, currentIndex: 0))
-            .environmentObject(AssessmentState(AssessmentObject(previewStep: example)))
+        Group {
+            InstructionStepView(InstructionState(example2, parentId: nil))
+                .environmentObject(PagedNavigationViewModel(pageCount: 5, currentIndex: 0))
+            .environmentObject(AssessmentState(AssessmentObject(previewStep: example2)))
+            InstructionStepView(InstructionState(example1, parentId: nil), alignment: .center)
+                .environmentObject(PagedNavigationViewModel(pageCount: 5, currentIndex: 0))
+                .environmentObject(AssessmentState(AssessmentObject(previewStep: example1)))
+        }
     }
 }
 
-fileprivate let example = InstructionStepObject(
+fileprivate let example1 = InstructionStepObject(
     identifier: "example",
     title: "Example Survey A",
     detail: "You will be shown a series of example questions. This survey has no additional instructions.",
     imageInfo: SageResourceImage(.survey))
+
+fileprivate let example2 = InstructionStepObject(
+    identifier: "example",
+    title: "Example Survey A",
+    detail: "You will be shown a series of example questions. This survey has no additional instructions.",
+    imageInfo: FetchableImage(imageName: "survey.1", bundle: Bundle.module, placementHint: "iconAfter"))
