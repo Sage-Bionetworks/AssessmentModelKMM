036c67beda06a82b2a1f8e8aceb15ceb7b43c084
Nathaniel Brown <nathaniel.brown@sagebase.org>
2022-11-17 21:50:18 -0800

    Version 0.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ tasks.dokkaHtmlMultiModule {
 
 allprojects {
     group = "org.sagebionetworks.assessmentmodel"
-    version = "0.10.0"
+    version = "0.10.1"
 
     repositories {
         google()
