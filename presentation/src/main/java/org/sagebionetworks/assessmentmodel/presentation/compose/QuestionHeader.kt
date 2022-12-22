8805baa9eed09baad08848c8250a6e0e096b36e3
Aaron Rabara <aaron.rabara@sagebase.org>
2022-12-22 14:17:36 -0800

    Added collapsable header for choice questions that are scrollable

--- presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/compose/QuestionHeader.kt
+++ presentation/src/main/java/org/sagebionetworks/assessmentmodel/presentation/compose/QuestionHeader.kt
@@ -1,28 +1,26 @@
 package org.sagebionetworks.assessmentmodel.presentation.compose
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import org.sagebionetworks.assessmentmodel.presentation.AssessmentViewModel
 import org.sagebionetworks.assessmentmodel.presentation.ui.theme.BackgroundGray
 import org.sagebionetworks.assessmentmodel.presentation.ui.theme.sageH1
 import org.sagebionetworks.assessmentmodel.presentation.ui.theme.sageP2
-import org.sagebionetworks.assessmentmodel.survey.ChoiceQuestion
-import org.sagebionetworks.assessmentmodel.survey.QuestionState
+import java.lang.Integer.max
 
 @Composable
 internal fun QuestionHeader(
@@ -31,13 +29,30 @@ internal fun QuestionHeader(
     detail: String?,
     assessmentViewModel: AssessmentViewModel,
     scrollState: ScrollState,
+    headerCanCollapse: Boolean = false,
     modifier: Modifier = Modifier
 ) {
     val elevationValue = if (scrollState.value > 5) { 5.dp} else {0.dp}
     val headerElevation = animateDpAsState(targetValue = elevationValue)
+    val coroutineScope = rememberCoroutineScope()
+    val subtitleHeight = remember { mutableStateOf(0) }
+    val detailHeight = remember { mutableStateOf(0) }
+    val maxScroll = if (scrollState.maxValue > 3000) {
+        remember { mutableStateOf(0) }
+    } else {
+        remember { mutableStateOf(scrollState.maxValue) }
+    }
+    maxScroll.value = max(scrollState.maxValue, maxScroll.value)
+    val fullHeaderShouldDisplay = !headerCanCollapse ||
+            scrollState.value == 0 ||
+            maxScroll.value < (subtitleHeight.value + detailHeight.value)
     Card(
         elevation = headerElevation.value,
-        modifier = Modifier.padding(bottom = 4.dp)
+        modifier = Modifier
+            .padding(bottom = 4.dp)
+            .clickable {
+                coroutineScope.launch { scrollState.scrollTo(0) }
+            }
     ) {
 
         Column(
@@ -60,14 +75,17 @@ internal fun QuestionHeader(
                     .padding(start = 20.dp, end = 20.dp)
             ) {
                 Spacer(modifier = Modifier.height(24.dp))
-                subtitle?.let { subtitle ->
-                    Text(
-                        text = subtitle,
-                        style = sageP2,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 0.dp, bottom = 0.dp, start = 24.dp, end = 8.dp)
-                    )
+                AnimatedVisibility(visible = fullHeaderShouldDisplay) {
+                    subtitle?.let { subtitle ->
+                        Text(
+                            text = subtitle,
+                            style = sageP2,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 0.dp, bottom = 0.dp, start = 24.dp, end = 8.dp)
+                                .onGloballyPositioned { subtitleHeight.value = it.size.height }
+                        )
+                    }
                 }
                 title?.let { title ->
                     Spacer(modifier = Modifier.height(16.dp))
@@ -79,15 +97,21 @@ internal fun QuestionHeader(
                             .padding(top = 0.dp, bottom = 0.dp, start = 24.dp, end = 8.dp)
                     )
                 }
-                detail?.let { detail ->
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = detail,
-                        style = sageP2,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 0.dp, bottom = 0.dp, start = 24.dp, end = 8.dp)
-                    )
+                AnimatedVisibility(visible = fullHeaderShouldDisplay) {
+                    detail?.let { detail ->
+                        Column(modifier = Modifier
+                            .onGloballyPositioned { detailHeight.value = it.size.height }
+                        ) {
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Text(
+                                text = detail,
+                                style = sageP2,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 0.dp, bottom = 0.dp, start = 24.dp, end = 8.dp)
+                            )
+                        }
+                    }
                 }
                 Spacer(modifier = Modifier.height(16.dp))
             }
