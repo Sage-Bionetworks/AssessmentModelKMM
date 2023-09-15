c69c70ccec06a41936a748057933922e94521d26
Shannon Young <shannon.young@sagebase.org>
2023-09-14 17:07:43 -0700

    Version 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ tasks.dokkaHtmlMultiModule {
 allprojects {
     group = "org.sagebionetworks.assessmentmodel"
 
-    version = "1.0.2"
+    version = "1.0.3"
 
     repositories {
         google()
