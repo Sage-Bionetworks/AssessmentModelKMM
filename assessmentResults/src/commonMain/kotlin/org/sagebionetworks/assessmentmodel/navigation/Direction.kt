2c2ef7c50914f5c3ae321abae01197548ce11662
Nathaniel Brown <nathaniel.brown@sagebase.org>
2023-11-01 13:02:48 -0700

    Put back lowercase @SerialName for Direction enum

--- assessmentResults/src/commonMain/kotlin/org/sagebionetworks/assessmentmodel/navigation/Direction.kt
+++ assessmentResults/src/commonMain/kotlin/org/sagebionetworks/assessmentmodel/navigation/Direction.kt
@@ -1,5 +1,6 @@
 package org.sagebionetworks.assessmentmodel.navigation
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,14 +8,17 @@ enum class Direction {
     /**
      * Move forward through the assessment.
      */
+    @SerialName("forward")
     Forward,
     /**
      * Move backward through the assessment.
      */
+    @SerialName("backward")
     Backward,
     /**
      * Exit the assessment early. If this direction indicator is set, then the entire assessment run should end.
      */
+    @SerialName("exit")
     Exit,
     ;
 }
